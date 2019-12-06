e89505f56a6bfe6eae57a8c0e575b4cc19f99f9a
tygedavis <tyge.davis@gmail.com>
2019-12-06 09:17:20 -0700

    API call made and data recieve

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  state = {
+    players: []
+  }
+
+  componentDidMount() {
+    console.log('CDM Invoked');
+
+    axios.get('http://localhost:5000/api/players')
+    .then(res => {
+      console.log('Initial Player Data: ', res.data);
+      this.setState({ players: [...this.state.players, res.data]});
+      console.log('First State Update: ', this.state.players);
+    });
+  };
+
+  componentDidUpdate() {
+    console.log('CDU Invoked')
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Womens World Cup</h1>
+        <div className='playerCard'>
+        
+        </div>
+
+      </div>
+    );
+  };
+};
 
 export default App;
